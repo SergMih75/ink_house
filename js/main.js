5c1849837f4453db7690d272d6796981d54060cb
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2023-11-07 22:28:50 +0300

    add section about and footer

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
 let collection = document.querySelectorAll('.collection-item')
+let collectionFooter = document.querySelectorAll('.footer-collection')
+
 collection.forEach(function (elem) {
 	elem.addEventListener('click', activeCollection)
 })
 
-// Первоначальное формирование списка репродукций в зависимости от активной кнопки коллекции при загрузке страницы
-document.addEventListener('DOMContentLoaded', function(){
-    collection.forEach(function (elem) {
-        if (elem.classList.contains('collection-active')){
-            let collectionAttrib = elem.getAttribute('data-attr')
+// Активация кнопки выбора коллекции и заполнение карточек коллекции из меню footer
+collectionFooter.forEach(function (elem) {
+	elem.addEventListener('click', function () {
+		let attr = this.getAttribute('data-attr')
+		collection.forEach(function (el) {
+			el.classList.remove('collection-active')
+			let collectionAttr = el.getAttribute('data-attr')
+			if (attr === collectionAttr) {
+				el.classList.add('collection-active')
 
-    if (collectionAttrib == 'fr') {
-        creationCard(fr)
-    }
-    if (collectionAttrib == 'ddr') {
-        creationCard(ddr)
-    }
-    if (collectionAttrib == 'gb') {
-        creationCard(gb)
-    }
-        }
-    })
+				if (collectionAttr == 'fr') {
+					creationCard(fr)
+				}
+				if (collectionAttr == 'ddr') {
+					creationCard(ddr)
+				}
+				if (collectionAttr == 'gb') {
+					creationCard(gb)
+				}
+			}
+		})
+	})
+})
+// Первоначальное формирование списка репродукций в зависимости от активной кнопки коллекции при загрузке страницы
+document.addEventListener('DOMContentLoaded', function () {
+	collection.forEach(function (elem) {
+		if (elem.classList.contains('collection-active')) {
+			let collectionAttrib = elem.getAttribute('data-attr')
+
+			if (collectionAttrib == 'fr') {
+				creationCard(fr)
+			}
+			if (collectionAttrib == 'ddr') {
+				creationCard(ddr)
+			}
+			if (collectionAttrib == 'gb') {
+				creationCard(gb)
+			}
+		}
+	})
 })
 
 function activeCollection() {
@@ -29,25 +54,25 @@ function activeCollection() {
 	this.classList.add('collection-active')
 	let collectionAttrib = this.getAttribute('data-attr')
 
-    if (collectionAttrib == 'fr') {
-        creationCard(fr)
-    }
-    if (collectionAttrib == 'ddr') {
-        creationCard(ddr)
-    }
-    if (collectionAttrib == 'gb') {
-        creationCard(gb)
-    }
+	if (collectionAttrib == 'fr') {
+		creationCard(fr)
+	}
+	if (collectionAttrib == 'ddr') {
+		creationCard(ddr)
+	}
+	if (collectionAttrib == 'gb') {
+		creationCard(gb)
+	}
 }
 
-function creationCard(data){
-    document.querySelector('.reproduction-collection').innerHTML=""
+function creationCard(data) {
+	document.querySelector('.reproduction-collection').innerHTML = ''
 
-    data.forEach(function(item){
-    let cardItem = document.createElement('div')
-    cardItem.classList.add('reproduction-collection-item')
+	data.forEach(function (item) {
+		let cardItem = document.createElement('div')
+		cardItem.classList.add('reproduction-collection-item')
 
-    cardItem.innerHTML = `
+		cardItem.innerHTML = `
         <img class="item-img" src="${item[0]}" alt="">
         <h3 class="item-author">${item[1]}</h3>
         <h2 class="item-title">${item[2]}</h2>
@@ -56,13 +81,11 @@ function creationCard(data){
         <div class="item-add-btn" data-attr="${item[5]}">В корзину</div>
     `
 
-    document.querySelector('.reproduction-collection').append(cardItem)
-    })
+		document.querySelector('.reproduction-collection').append(cardItem)
+	})
 
-        let addBtn = document.querySelectorAll('.item-add-btn')
-        addBtn.forEach(function(elem){
-            let productId = elem.getAttribute('data-attr')
-            console.log('productId: ', productId);
-        })
-// console.log('addBtn: ', addBtn);
+	let addBtn = document.querySelectorAll('.item-add-btn')
+	addBtn.forEach(function (elem) {
+		let productId = elem.getAttribute('data-attr')
+	})
 }
