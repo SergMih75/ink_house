ee6b5826907032d78ec0c6469d0ca80ce6c375e6
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2024-09-23 00:02:12 +0300

    add LocalStorage functions

--- js/basket.js
+++ js/basket.js
@@ -1,6 +1,5 @@
 // ! Открытие окна с корзиной
-document.querySelector('.basket').addEventListener('click', function (basket) {
-	console.log(basket)
+document.querySelector('.basket').addEventListener('click', function () {
 	document.querySelector('.basketpopup').classList.add('basketpopup-active')
 	document.body.style.overflow = 'hidden'
 
@@ -21,21 +20,49 @@ document.querySelector('.basketpopup-close').addEventListener('click', () => {
 	document.querySelector('.basketpopup-field').style.opacity = 0
 	document.querySelector('.basketpopup-close').style.opacity = 0
 	document.querySelector('.basketpopup-table').innerHTML = ''
+	basket = []
 })
 
 //  ! Заполнение корзины
 count = 0
 function addBasket() {
+
+	for (i=0; i <localStorage.length; i++) {
+	if (localStorage.key(i).slice(0,8) === 'product_') {
+		let orderName  = localStorage.key(i).slice(8)
+
+		fr.forEach(item=>{
+			if (orderName === item[5]) {
+				basket.push(item)
+			}
+		})
+
+		de.forEach(item=>{
+			if (orderName === item[5]) {
+				basket.push(item)
+			}
+		})
+
+		gb.forEach(item=>{
+			if (orderName === item[5]) {
+				basket.push(item)
+			}
+		})
+	}
+}
+
+
+
 	document.querySelector('.basketpopup-table').innerHTML = ''
 	rowHeader = document.createElement('tr')
 
 	rowHeader.innerHTML = `
-    <th>#</th>
+
 	<th>Рис.</th>
 	<th>Автор</th>
 	<th>Название</th>
 	<th>Описание</th>
-	<th>Количество</th>
+	<th>Кол-во</th>
 	<th>Цена</th>
 	<th>Сумма</th>
     `
@@ -45,14 +72,46 @@ function addBasket() {
 		row = document.createElement('tr')
 
 		row.innerHTML = `
-        <td>${(count = count + 1)}</td>
-        <td><img src="${item[0]}" alt="${item[2]}"></td> 
+        <td><img class="basket-img" src="${item[0]}" alt="${item[2]}"></td> 
         <td>${item[1]}</td>
         <td>${item[2]}</td>
         <td>${item[3]}</td>
-        <td></td>
+        <td>
+			<div class="quantity">
+				<div class="quantity-btn" data-attr="dec">+</div>
+				<span class="quantity-counter">1</span>
+				<div class="quantity-btn" data-attr="inc">-</div>
+			</div>
+		</td>
         <td>${item[4]}</td>
+		<td>${item[4]}</td>
     `
+
 		document.querySelector('.basketpopup-table').append(row)
 	})
+	let quantityBtn = document.querySelectorAll('.quantity-btn')
+
+quantityBtn.forEach((quantityBtn)=>{
+	quantityBtn.addEventListener('click',()=>{
+		let counter = 0
+		let sum
+		if (quantityBtn.nextElementSibling !== null) {
+			counter = +quantityBtn.nextElementSibling.textContent + 1
+			quantityBtn.nextElementSibling.innerHTML=counter
+
+			sum = counter * (+quantityBtn.parentElement.parentElement.nextElementSibling.textContent)
+			quantityBtn.parentElement.parentElement.nextElementSibling.nextElementSibling.textContent = sum
+
+		}
+		else{
+			if (+quantityBtn.previousElementSibling.textContent >0) {
+				counter = +quantityBtn.previousElementSibling.textContent - 1
+				quantityBtn.previousElementSibling.innerHTML=counter
+
+				sum = counter * (+quantityBtn.parentElement.parentElement.nextElementSibling.textContent)
+				quantityBtn.parentElement.parentElement.nextElementSibling.nextElementSibling.textContent = sum
+			}
+		}
+	})
+})
 }
--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ collectionFooter.forEach(function (elem) {
 })
 // Первоначальное формирование списка репродукций в зависимости от активной кнопки коллекции при загрузке страницы
 document.addEventListener('DOMContentLoaded', function () {
+
 	collection.forEach(function (elem) {
 		if (elem.classList.contains('collection-active')) {
 			let collectionAttrib = elem.getAttribute('data-attr')
@@ -46,6 +47,12 @@ document.addEventListener('DOMContentLoaded', function () {
 			}
 		}
 	})
+
+	for (i=0; i <localStorage.length; i++) {
+	if (localStorage.key(i).slice(0,8) === 'product_') {
+		document.querySelector('.full-basket').classList.add('full-basket-add')
+	}
+}
 })
 
 function activeCollection() {
@@ -67,20 +74,41 @@ function activeCollection() {
 }
 
 function creationCard(data) {
+	console.log(data);
 	document.querySelector('.reproduction-collection').innerHTML = ''
 
 	data.forEach(function (item) {
 		let cardItem = document.createElement('div')
 		cardItem.classList.add('reproduction-collection-item')
 
-		cardItem.innerHTML = `
-        <img class="item-img" src="${item[0]}" alt="">
+		if (localStorage.getItem('product_'+item[5]) == null) {
+			cardItem.innerHTML = `
+		<div class="item-img-mask">
+			<img class="item-img" src="${item[0]}" alt="">
+		</div>
         <h3 class="item-author">${item[1]}</h3>
         <h2 class="item-title">${item[2]}</h2>
         <p class="item-description">${item[3]}</p>
         <p class="item-price">${item[4]} руб</p>
-        <div class="item-add-btn" data-attr="${item[5]}">В корзину</div>
+		<div class="btn-block">
+		<div class="item-add-btn" data-attr="${item[5]}">Добавить</div>
+		<div class="item-add-btn__remove  item-add-btn__remove-disable" data-attr="${item[5]}">Удалить</div>
+		</div>
     `
+		} else
+		{cardItem.innerHTML = `
+		<div class="item-img-mask">
+			<img class="item-img" src="${item[0]}" alt="">
+		</div>
+        <h3 class="item-author">${item[1]}</h3>
+        <h2 class="item-title">${item[2]}</h2>
+        <p class="item-description">${item[3]}</p>
+        <p class="item-price">${item[4]} руб</p>
+		<div class="btn-block">
+		<div class="item-add-btn  item-add-btn__selected" data-attr="${item[5]}">Добавлено</div>
+		<div class="item-add-btn__remove item-add-btn__remove-active" data-attr="${item[5]}">Удалить</div>
+		</div>
+    `}
 
 		document.querySelector('.reproduction-collection').append(cardItem)
 	})
@@ -88,17 +116,35 @@ function creationCard(data) {
 	let addBtn = document.querySelectorAll('.item-add-btn')
 	addBtn.forEach(function (elem) {
 		elem.addEventListener('click', function () {
-			elem.innerHTML = 'Выбрано'
+			if (!elem.classList.contains('item-add-btn__selected')) {
+				elem.innerHTML = 'Добавлено'
 			elem.classList.add('item-add-btn__selected')
-			let productId = elem.getAttribute('data-attr')
-			data.forEach(function (el) {
-				if (productId === el[5]) {
-					basket.push(el)
-					document
-						.querySelector('.full-basket')
-						.classList.add('full-basket-add')
-				}
-			})
+
+			elem.nextElementSibling.classList.add('item-add-btn__remove-active')
+
+			localStorage.setItem('product_'+elem.getAttribute('data-attr'), elem.getAttribute('data-attr'))
+			localStorage.setItem('productQuantity_'+elem.getAttribute('data-attr'), 1)
+
+			if (!document.querySelector('.full-basket').classList.contains('full-basket-add')) {
+				document.querySelector('.full-basket').classList.add('full-basket-add')
+			}
+			}
+		})
+	})
+
+	let removeBtn = document.querySelectorAll('.item-add-btn__remove')
+
+	removeBtn.forEach((removeBtn)=>{
+		removeBtn.addEventListener('click', ()=> {
+			removeBtn.classList.remove('item-add-btn__remove-active')
+			removeBtn.previousElementSibling.classList.remove('item-add-btn__selected')
+			removeBtn.previousElementSibling.textContent = 'Добавить'
+			localStorage.removeItem('product_'+removeBtn.getAttribute('data-attr'))
+			localStorage.removeItem('productQuantity_'+removeBtn.getAttribute('data-attr'))
+
+			if (!removeBtn.classList.contains('item-add-btn__remove-disable')) {
+				removeBtn.classList.add('item-add-btn__remove-disable')
+			}
 		})
 	})
 }
